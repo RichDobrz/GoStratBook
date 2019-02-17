18a8298e4d7c807f2437350fb9c97d5cb70f57a6
RichDobrz <rjdobrz@gmail.com>
2019-02-17 10:43:01 -0500

    testing mouse offset and click event handlers

--- canvas.js
+++ canvas.js
@@ -1,13 +1,13 @@
-var canvas = document.getElementById('myCanvas');
-var ctx = canvas.getContext('2d');
+// var canvas = document.getElementById('myCanvas');
+// var ctx = canvas.getContext('2d');
 
-var painting = document.getElementById("paint")
-var paintStyle = getComputedStyle(painting)
+// var painting = document.getElementById("paint")
+// var paintStyle = getComputedStyle(painting)
 
-var mouse = {
-    x:0,
-    y:0
-}
+// var mouse = {
+//     x:0,
+//     y:0
+// }
 
 // canvas.addEventListener("mousemove", function(e){
 //     mouse.x = e.pageX - this.offsetLeft;
@@ -35,25 +35,49 @@ var mouse = {
 //     ctx.stroke();
 // }
 
-canvas.addEventListener('mousedown', mHandler);
+// canvas.addEventListener('mousedown', mHandler);
 
-function mHandler(event) {
+// function mHandler(event) {
     
-    if(event.offsetX && event.offsetY) {
-        mouse.x = event.offsetX;
-        mouse.y = event.offsetY;
-    }
-    else {
-        mouse.x = event.layerX;
-        mouse.y = event.layerY;
-    }
-};
+//     if(event.offsetX && event.offsetY) {
+//         mouse.x = event.offsetX;
+//         mouse.y = event.offsetY;
+//     }
+//     else {
+//         mouse.x = event.layerX;
+//         mouse.y = event.layerY;
+//     }
+// };
 
-function main() {
-    ctx.clearRect(0, 0, ctx.width, ctx.height);
-    ctx.fillStyle = "red";
-    ctx.fillRect(mouse.x -25, mouse.y -25, 50, 50);
+// function main() {
+//     ctx.clearRect(0, 0, ctx.width, ctx.height);
+//     ctx.fillStyle = "gray";
+//     ctx.fillRect(mouse.x -25, mouse.y -25, 50, 50);
+// }
+
+// setInterval(main, 1000 / 60);
+
+$("#mapImg").click(function() {
+    var position = $(this).offset()
+    $("#smoke").offset(position).show()
+})
+
+function doSomething(e) {
+	var posx = 0;
+	var posy = 0;
+	if (!e) var e = window.event;
+	if (e.pageX || e.pageY) 	{
+		posx = e.pageX;
+		posy = e.pageY;
+	}
+	else if (e.clientX || e.clientY) 	{
+		posx = e.clientX + document.body.scrollLeft
+			+ document.documentElement.scrollLeft;
+		posy = e.clientY + document.body.scrollTop
+			+ document.documentElement.scrollTop;
+    }
+    $("#mapImg").click(function() {
+        $("#smoke").offset({top: posx, left: posy}).show()
+    })
+	
 }
-
-setInterval(main, 1000 / 60);
-
